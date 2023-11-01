6544727b70de9867c65b910758da631a5dd0cc5b
Flokkq <98653095+Flokkq@users.noreply.github.com>
2023-11-01 15:40:25 +0100

    added test structure for API

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 use email_newsletter::run;
 
 #[tokio::main]
-pub async fn main() -> Result<(), std::io::Error> {
-    run().await
+async fn main() -> Result<(), std::io::Error> {
+    let listener = std::net::TcpListener::bind("127.0.0.1:8080")?;
+    run(listener)?.await?;
+    Ok(())
 }
-// page: 50
