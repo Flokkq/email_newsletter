0e0495bacb2235d5531224fd23f78aae8b843227
Flokkq <webcla21@htl-kaindorf.at>
2023-12-09 11:43:58 +0100

    cleaned up code, isolated tests

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,17 @@
+use std::net::TcpListener;
+use sqlx::{Connection, PgPool};
 use email_newsletter::startup::run;
+use email_newsletter::configuration::{get_configuration, Settings};
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    let listener = std::net::TcpListener::bind("127.0.0.1:8080")?;
-    run(listener)?.await?;
-    Ok(())
+    let configuration: Settings = get_configuration().expect("Failed to read configuration.");
+    let connection_pool = PgPool::connect(
+        &configuration.database.connection_string()
+    ).await.expect("Failed to connect to Postgres");
+
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let listener = TcpListener::bind(address)?;
+    run(listener, connection_pool)?.await
 }
-// Page 84
+// 4 TELEMETRY
