8a8254444864721f2971dbb2a88631f079244687
Flokkq <webcla21@htl-kaindorf.at>
2023-12-26 20:33:15 +0100

    workflow test3

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -18,7 +18,7 @@ pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> Ht
     );
     let _request_span_guard = request_span.enter();
 
-    let query_span = tracing::info_span!("Saving new subscriber details in the databases");
+    let query_span = tracing::info_span!("Saving new subscriber details in the database");
 
     match sqlx::query!(
         r#"
